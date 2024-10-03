35a56ac110d0bab5e571c2a91622a1461e965b24
sanky2003 <98749917+sanky2003@users.noreply.github.com>
2024-10-03 10:49:54 +0000

    datatypes file commit

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as  the newer version
+
+//alert(3+3) //this will show an error on node js not on browser
+
+let name="sanket" //string
+let age=18;
+let state;
+let value=false;
+
+//datatypes
+//number=> 2power53
+//bigint
+//string=>""
+//boolean
+//null
+//undefined 
+//symbol =>used for the uniqueness
+//this all are primitive datatypes
+
+//objects
+
+console.log(typeof "sanket") //=>output will be string
+//type of null is object remeber
+console.log(typeof undefined)//=> undefined
+console.log(typeof null)//=> objects
+
